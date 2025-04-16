d4f0a67be7e5043d22468c5ad134441464170f86
William Orbach <worbach@wgu.edu>
2025-04-16 02:01:44 -0400

    Fixed contact.jsx to ensure proper mobile viewing.

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -35,7 +35,7 @@ export const Contact = () => {
         >
 
             <RevealOnScroll>
-                <div className="px-4 w-150">
+                <div className="w-full sm:w-full md:w-[600px] px-4 md:px-10 mx-auto">
 
                     <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text
                 text-transparent text-center"> Get In Touch</h2>
